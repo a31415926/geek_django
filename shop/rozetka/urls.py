e2d592af908f561645a95db7252531c66da6fdb2
a31415926 <65874366+a31415926@users.noreply.github.com>
2021-01-30 17:40:14 +0200

    Delete shop/rozetka directory

--- shop/rozetka/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rozetka.views import *
-
-
-urlpatterns = [
-    path('', main_page, name='rozetka_main_page'),
-    path('cats/<int:cat_id>/', goods_of_cats, name='goods_list_cats_page'),
-    path('parser/', parser_goods, name='parser_page'),
-]
