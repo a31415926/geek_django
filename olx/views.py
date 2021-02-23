2774f4f17594ac4adb3daaba193b50a7c0df828e
John Doe <johndoe@example.com>
2021-02-23 12:50:13 +0200

    add parser with multiprocessing

--- olx/views.py
+++ olx/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import subprocess
 from olx.models import ScrapeTask
+from multiprocessing import Process
+from olx import parser
 
 def parse_page(requests):
 
@@ -8,11 +10,12 @@ def parse_page(requests):
         link_cat = requests.POST.get('link')
         qty = requests.POST.get('qty')
         if int(qty)>0:
-            sub_id = ScrapeTask.objects.create(
+            proccess_parse = Process(target=parser.main, args=(link_cat, qty))
+            proccess_parse.start()
+            """sub_id = ScrapeTask.objects.create(
                 link = link_cat, 
                 qty=qty,
             )
-            sub = subprocess.Popen(['python', 'parse_olx.py', link_cat, qty, str(sub_id.pk)], shell=True)
+            sub = subprocess.Popen(['python', 'parse_olx.py', link_cat, qty, str(sub_id.pk)], shell=True)"""
 
     return render(requests, 'olx/parse.html')
-# Create your views here.
